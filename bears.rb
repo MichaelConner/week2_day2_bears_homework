996489761358f17293014550600174dd8213993b
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-27 19:12:03 +0000

    initial commit

---
+++ bears.rb
@@ -0,0 +1,11 @@
+class bears
+
+def initialize()
+end
+
+
+
+
+
+
+end
---
+++ specs/bears_specs.rb
@@ -0,0 +1,16 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bears')
+
+class TestBears < MiniTest::Test
+
+def setup
+end
+
+
+
+
+
+
+
+end
